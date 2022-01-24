859c07aad5f86cab38dd39ddf9259c09de111910
BowDown097 <42720004+BowDown097@users.noreply.github.com>
2022-01-23 16:13:04 -0800

    Glaive God -> Less crashes hopefully?

--- Bloons-TD-Battles-2/Lua/GlaiveGod/start.lua
+++ Bloons-TD-Battles-2/Lua/GlaiveGod/start.lua
@@ -1,13 +1,27 @@
 mod_metadata("name", "Glaive God")
+mod_metadata("author", "BowDown097")
+mod_metadata("version", "1.0.1")
 
-local function patch_glaive_count(tower)
+local function patch_orbiting_glaives(tower)
+    for _, variable in pairs(tower["variables"]) do
+        if variable["key"] == "orbiting_glaives_damage" then
+            variable["variable"]["value"] = 10.0
+        end
+    end
     for _, node in pairs(tower["nodes"]) do
         if node["name"] == "Orbiting Glaives VFX Launcher" then
-            node["props"]["emission_props"]["concurrent_projectile_limit"] = 10000
+            node["props"]["emission_props"]["concurrent_projectile_limit"] = 100
+        end
+        if node["name"] == "LOC_UPGRADE_NAME_Red_Hot_Rangs" then
+            for _, modifier in pairs(node["variable_modifiers"]) do
+                if modifier["key"] == "orbiting_glaives_damage" then
+                    modifier["expression"] = tostring(11.0)
+                end
+            end
         end
     end
 
     return tower
 end
 
-jet_json_patch("game_data/game_project/assets/towers/boomerang_monkey/data/boomerang_monkey.tower_blueprint", patch_glaive_count)
+jet_json_patch("game_data/game_project/assets/towers/boomerang_monkey/data/boomerang_monkey.tower_blueprint", patch_orbiting_glaives)
