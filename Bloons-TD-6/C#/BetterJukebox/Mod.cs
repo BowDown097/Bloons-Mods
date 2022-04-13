d9930d1f91a288bbbe6304799b586e03c7a0c5b9
BowDown097 <btd6modsyeet@gmail.com>
2022-04-13 06:13:10 -0700

    Fixes

--- Bloons-TD-6/C#/BetterJukebox/Mod.cs
+++ Bloons-TD-6/C#/BetterJukebox/Mod.cs
@@ -31,48 +31,41 @@ public class Mod : BloonsTD6Mod
         if (!Directory.Exists(jukeboxFolder))
             Directory.CreateDirectory(jukeboxFolder);
 
-        foreach (string mp3 in Directory.GetFiles(jukeboxFolder))
+        foreach (string mp3 in Directory.GetFiles(jukeboxFolder, "*.mp3"))
         {
-            try
+            string samplesFile = mp3.Replace("mp3", "samples");
+            string infoFile = mp3.Replace("mp3", "mpeginfo");
+            string filename = Path.GetFileNameWithoutExtension(new FileInfo(mp3).Name);
+            BinaryFormatter fmt = new();
+
+            if (File.Exists(samplesFile) && File.Exists(infoFile) && enableCache)
             {
-                string samplesFile = mp3.Replace("mp3", "samples");
-                string infoFile = mp3.Replace("mp3", "mpeginfo");
-                string filename = Path.GetFileNameWithoutExtension(new FileInfo(mp3).Name);
-                BinaryFormatter fmt = new();
-
-                if (File.Exists(samplesFile) && File.Exists(infoFile) && enableCache)
-                {
-                    MelonLogger.Msg($"Reading from cache for {filename}");
-                    using FileStream fs = new(samplesFile, FileMode.Open);
-                    float[] fsSamples = (float[])fmt.Deserialize(fs);
-                    JObject mpegInfo = JObject.Parse(File.ReadAllText(infoFile));
-                    int channels = mpegInfo["channels"]?.Value<int>() ?? 0;
-                    int sampleRate = mpegInfo["sampleRate"]?.Value<int>() ?? 0;
-                    AudioClip fsClip = AudioClip.Create(filename, fsSamples.Length / 2, channels, sampleRate, false);
-                    fsClip.SetData(fsSamples, 1);
-                    clips.Add(fsClip);
-                    return;
-                }
-
-                MelonLogger.Msg($"Reading directly for {filename}");
-                using MpegFile mpegFile = new(mp3);
-                float[] samples = new float[mpegFile.Length / mpegFile.Channels / 2];
-                mpegFile.ReadSamples(samples, 0, samples.Length);
-                AudioClip clip = AudioClip.Create(filename, samples.Length / 2, mpegFile.Channels, mpegFile.SampleRate, false);
-                clip.SetData(samples, 1);
-                clips.Add(clip);
-
-                if (enableCache)
-                {
-                    using FileStream samplesOut = new(samplesFile, FileMode.Create);
-                    fmt.Serialize(samplesOut, samples);
-                    JObject infoOut = new(new JProperty("channels", mpegFile.Channels), new JProperty("sampleRate", mpegFile.SampleRate));
-                    File.WriteAllText(infoFile, infoOut.ToString());
-                }
+                MelonLogger.Msg($"Reading from cache for {filename}");
+                using FileStream fs = new(samplesFile, FileMode.Open);
+                float[] fsSamples = (float[])fmt.Deserialize(fs);
+                JObject mpegInfo = JObject.Parse(File.ReadAllText(infoFile));
+                int channels = mpegInfo["channels"]?.Value<int>() ?? 0;
+                int sampleRate = mpegInfo["sampleRate"]?.Value<int>() ?? 0;
+                AudioClip fsClip = AudioClip.Create(filename, fsSamples.Length / 2, channels, sampleRate, false);
+                fsClip.SetData(fsSamples, 1);
+                clips.Add(fsClip);
+                return;
             }
-            catch (Exception exception)
+
+            MelonLogger.Msg($"Reading directly for {filename}");
+            using MpegFile mpegFile = new(mp3);
+            float[] samples = new float[mpegFile.Length / mpegFile.Channels / 2];
+            mpegFile.ReadSamples(samples, 0, samples.Length);
+            AudioClip clip = AudioClip.Create(filename, samples.Length / 2, mpegFile.Channels, mpegFile.SampleRate, false);
+            clip.SetData(samples, 1);
+            clips.Add(clip);
+
+            if (enableCache)
             {
-                MelonLogger.Error(exception.Message);
+                using FileStream samplesOut = new(samplesFile, FileMode.Create);
+                fmt.Serialize(samplesOut, samples);
+                JObject infoOut = new(new JProperty("channels", mpegFile.Channels), new JProperty("sampleRate", mpegFile.SampleRate));
+                File.WriteAllText(infoFile, infoOut.ToString());
             }
         }
     }
