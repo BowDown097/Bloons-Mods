2f8551414bcc0013df8b31d9c44369e13458307c
BowDown097 <btd6modsyeet@gmail.com>
2022-05-23 06:02:38 -0700

    Battles 2 -> Port Bananza

---
+++ Bloons-TD-Battles-2/JavaScript/Bananza/README.md
@@ -0,0 +1,8 @@
+# Bananza
+Replicates the Bananza mode from Battles 1.
+Originally made by Greenphx, ported by me
+
+## Information
+Release date: 5/23/2022
+
+Status: Working
---
+++ Bloons-TD-Battles-2/JavaScript/Bananza/main.js
@@ -0,0 +1,32 @@
+function patchBloonsets(data) {
+    for (let i = 0; i < data.length; i++) {
+        data[i]["Eco Change"] *= 2;
+    }
+
+    return { successful: true, data: data };
+}
+
+function patchFarm(data) {
+    const brf = data.nodes.filter(n => n.name == "LOC_UPGRADE_NAME_Banana_Research_Facility")[0];
+    const cenMarket = data.nodes.filter(n => n.name == "LOC_UPGRADE_NAME_Central_Market")[0];
+
+    const baseValue = data.variables.filter(v => v.key == "banana_value")[0];
+    const brfValue = brf.variable_modifiers.filter(m => m.key == "banana_value")[0];
+    const cenMarketValue = cenMarket.variable_modifiers.filter(m => m.key == "banana_value")[0];
+
+    baseValue.variable.value *= 2;
+    brfValue.expression *= 2;
+    cenMarketValue.expression *= 2;
+
+    return { successful: true, data: data };
+}
+
+function patchGameRules(data) {
+    data.starting_cash *= 2;
+    data.starting_income *= 2;
+    return { successful: true, data: data };
+}
+
+souped.registerCsvPatcher(patchBloonsets, "bloon_sets.bloonset");
+souped.registerJsonPatcher(patchFarm, "banana_farm.tower_blueprint");
+souped.registerJsonPatcher(patchGameRules, "*.gamerules");
