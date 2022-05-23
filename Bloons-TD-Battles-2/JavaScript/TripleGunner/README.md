4facea368587b80b6c1598aab9480222b5499685
BowDown097 <btd6modsyeet@gmail.com>
2022-05-23 06:08:13 -0700

    Battles 2 -> Port Triple Gunner

---
+++ Bloons-TD-Battles-2/JavaScript/TripleGunner/README.md
@@ -0,0 +1,8 @@
+# Triple Gunner
+The Dartling Gunner becomes a 3-round burst.
+Originally made by Vadmeme, ported by me
+
+## Information
+Release date: 5/23/2022
+
+Status: Working
---
+++ Bloons-TD-Battles-2/JavaScript/TripleGunner/main.js
@@ -0,0 +1,7 @@
+function patchDartling(data) {
+    const projCount = data.variables.filter(v => v.key == "proj_count")[0];
+    projCount.variable.value = 3;
+    return { successful: true, data: data };
+}
+
+souped.registerJsonPatcher(patchDartling, "dartling_gunner.tower_blueprint");
